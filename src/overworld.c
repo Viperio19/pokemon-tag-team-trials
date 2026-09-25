7dd8a285502abc69d535ce1318dc3954cc4bd0be
Viperio19 <viperio19yt@gmail.com>
2026-09-25 23:34:42 +0200

    Fix rock smash anim issues, don't save party anymore before and after multiplayer battle

--- src/overworld.c
+++ src/overworld.c
@@ -2989,7 +2989,12 @@ void TrySetPlayer2DirectionCommand(enum Player2Command command, enum Direction d
     EnqueuePlayer2CommandToSend(command, direction, LOCALID_PLAYER_2, 0);
 }
 
-void Enqueueplayer2commandEndRockSmash(void)
+void EnqueuePlayer2CommandUseRockSmash(void)
+{
+    EnqueuePlayer2CommandToSend(P2_CMD_USE_ROCK_SMASH, gFieldEffectArguments[2], VarGet(VAR_LAST_TALKED), 0);
+}
+
+void EnqueuePlayer2CommandEndRockSmash(void)
 {
     EnqueuePlayer2CommandToSend(P2_CMD_END_ROCK_SMASH, gFieldEffectArguments[2], VarGet(VAR_LAST_TALKED), 0);
 }
@@ -3229,13 +3234,9 @@ static void UpdateAllLinkPlayers(u16 *keys, s32 selfId)
         StartStrengthAnim(GetObjectEventIdByLocalId(arg1), arg2);
         break;
     case P2_CMD_USE_ROCK_SMASH:
-        ObjectEventSetGraphicsId(objEvent, GetPlayer2AvatarGraphicsIdByStateIdAndGender(PLAYER_AVATAR_STATE_SURFING, gSaveBlock2Ptr->player2Gender));
-        ObjectEventClearHeldMovementIfFinished(objEvent);
         ScriptMovement_StartObjectMovementScript(arg2, gSaveBlock1Ptr->location.mapNum, gSaveBlock1Ptr->location.mapGroup, Common_Movement_RockSmashBreak);
         break;
     case P2_CMD_END_ROCK_SMASH:
-        ObjectEventSetGraphicsId(objEvent, GetPlayer2AvatarGraphicsIdByStateIdAndGender(PLAYER_AVATAR_STATE_NORMAL, gSaveBlock2Ptr->player2Gender));
-        StartSpriteAnim(&gSprites[objEvent->spriteId], arg1);
         RemoveAnyObjectEventByLocalIdAndMap(arg2, gSaveBlock1Ptr->location.mapNum, gSaveBlock1Ptr->location.mapGroup);
         break;
     case P2_CMD_USE_SURF:
